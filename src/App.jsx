39a0fa2526617391bf700e37533a52bf2b4c444b
Ammar-Amin <ammaramin5253@gmail.com>
2024-04-27 11:45:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-8xl text-red-400 text-center">hello <i className="ri-thumb-up-fill"></i></h1>
+    </>
+  )
+}
+
+export default App
