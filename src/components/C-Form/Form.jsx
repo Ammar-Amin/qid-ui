3d91170627ae16af81a88212cd627d9100a8c2db
Ammar-Amin <ammaramin5253@gmail.com>
2024-04-29 17:46:20 +0530

    C-form page added

--- src/components/C-Form/Form.jsx
+++ src/components/C-Form/Form.jsx
@@ -1,10 +1,107 @@
 import React from 'react'
+import '../../index.css'
 
 function Form() {
     return (
-        <div className='mt-[100px]'>
-            <div className='text-9xl font-bold text-center text-green-500'>
-                Form Page
+        <div className='bg-[url("https://oneqid.com/wp-content/uploads/2023/09/grid-for-video.png")] bg-contain border-[1px] border-slate-600'>
+            <div className='pt-32 px-2 text-white bg-black/70 pb-16'>
+                {/* Section 1  */}
+                <div className='px-4 pt-4 md:mx-[60px] lg:mx-[100px] xl:mx-[130px]'>
+                    <h1 className='text-5xl font-bold md:text-6xl lg:text-7xl'><span className='bg-gradient-to-r from-orange-400 to-orange-500 text-transparent bg-clip-text'>C-Form</span> Automation</h1>
+                    <h2 className='my-3 text-2xl leading-7 font-medium text-gray-300 md:text-3xl'>Say Goodbye to Manual C-Forms</h2>
+                    <div className='my-6 font-bold flex flex-col gap-3 md:flex-row'>
+                        <div>
+                            <button className='px-3 py-2 bg-slate-50 text-black rounded-3xl lg:text-xl lg:font-medium hover:bg-black hover:text-white'>Book a demo</button>
+                        </div>
+                        <div>
+                            <button className='px-3 py-2 bg-black border-[1px] border-slate-800 rounded-3xl lg:text-xl lg:font-medium hover:bg-white hover:text-black'>
+                                <i class="ri-chrome-fill font-extralight text-lg mr-2"></i>
+                                Get chrome Extension
+                            </button>
+                        </div>
+                    </div>
+
+                </div>
+                {/* img 1 Moblie  */}
+                <div className='md:hidden'>
+                    <img decoding="async" width="2024" height="3393" src="https://oneqid.com/wp-content/uploads/2023/08/c-form-long.png" class="attachment-full size-full wp-image-992" alt="" srcset="https://oneqid.com/wp-content/uploads/2023/08/c-form-long.png 2024w, https://oneqid.com/wp-content/uploads/2023/08/c-form-long-179x300.png 179w, https://oneqid.com/wp-content/uploads/2023/08/c-form-long-611x1024.png 611w, https://oneqid.com/wp-content/uploads/2023/08/c-form-long-768x1287.png 768w, https://oneqid.com/wp-content/uploads/2023/08/c-form-long-916x1536.png 916w, https://oneqid.com/wp-content/uploads/2023/08/c-form-long-1222x2048.png 1222w" sizes="(max-width: 2024px) 100vw, 2024px" />
+                </div>
+                {/* img 1 PC */}
+                <div className='hidden md:block md:pl-9'>
+                    <img fetchpriority="high" decoding="async" width="4048" height="2138" src="https://oneqid.com/wp-content/uploads/2023/07/Asset-4.png" class="attachment-full size-full wp-image-895" alt="" srcSet="https://oneqid.com/wp-content/uploads/2023/07/Asset-4.png 4048w, https://oneqid.com/wp-content/uploads/2023/07/Asset-4-300x158.png 300w, https://oneqid.com/wp-content/uploads/2023/07/Asset-4-1024x541.png 1024w, https://oneqid.com/wp-content/uploads/2023/07/Asset-4-768x406.png 768w, https://oneqid.com/wp-content/uploads/2023/07/Asset-4-1536x811.png 1536w, https://oneqid.com/wp-content/uploads/2023/07/Asset-4-2048x1082.png 2048w" sizes="(max-width: 4048px) 100vw, 4048px"></img>
+                </div>
+                {/* Section 2  */}
+                <div className='p-4 md:mx-[50px] lg:mx-[100px] xl:mx[130px]'>
+                    <h1 className='my-4 text-4xl font-bold md:text-6xl lg:text-7xl'>
+                        Trust us, it's truly a matter of just
+                        <span className='bg-gradient-to-r from-yellow-200 to-orange-500 text-transparent bg-clip-text'> one click.</span>
+                    </h1>
+                    <div className='my-4 text-xl font-medium flex flex-col gap-3 md:flex-row lg:text-2xl'>
+                        <div className='p-4 bg-gradient-to-tr to-[#292929] from-[#000] border-2 border-slate-800 rounded-3xl'>
+                            Your guests upload photos of their Passport and Visa.
+                        </div>
+                        <div className='p-4 bg-gradient-to-tr to-[#292929] from-[#000]  border-2 border-slate-800 rounded-3xl'>
+                            Qid's AI engine extracts details like
+                            Passport Number, Visa Number, Name, Country, and Photos from the images
+                        </div>
+                        <div className='p-4 bg-gradient-to-tr to-[#292929] from-[#000]  border-2 border-slate-800 rounded-3xl'>
+                            Go to the C-Form website
+                            Open the qid C-Form Pro Chrome plugin.
+                            Select the guest. Done!
+                        </div>
+                    </div>
+                </div>
+                {/* Section 3  */}
+                <div className='mb-10 flex flex-col md:flex-row-reverse md:items-center md:mx-[50px] lg:mx-[120px] xl:mx[150px]'>
+                    {/* img 2 mobile version  */}
+                    <img decoding="async" width="714" height="1062" src="https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM.png" className="md:hidden rounded-3xl attachment-full size-full wp-image-917" alt="" srcSet="https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM.png 714w, https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM-202x300.png 202w, https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM-688x1024.png 688w" sizes="(max-width: 714px) 100vw, 714px"></img>
+
+                    {/* img 2 PC version */}
+                    <div className='w-[80%] mx-auto md:w-[40%]'>
+                        <div className='hidden md:block'>
+                            <div class="tilting-card-wrapper">
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="mouse-position-tracker"></div>
+                                <div class="tilting-card-body">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    {/* <div className='w-[80%] mx-auto md:w-[40%]'>
+                        <img decoding="async" width="714" height="1062" src="https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM.png" className="rounded-3xl attachment-full size-full wp-image-917" alt="" srcSet="https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM.png 714w, https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM-202x300.png 202w, https://oneqid.com/wp-content/uploads/2023/07/Screenshot-2023-07-27-at-12.23.51-PM-688x1024.png 688w" sizes="(max-width: 714px) 100vw, 714px"></img>
+                    </div> */}
+                    <div className='p-4 md:w-[60%]'>
+                        <h1 className='text-2xl font-medium md:text-4xl lg:text-5xl'>Elevate C-Form Processing with qid's State-of-the-Art Chrome Extension</h1>
+                        <h2 className='my-4 text-[18px] text-slate-300 md:text-xl lg:text-2xl'>Simplify C-Form management like never before with qid’s revolutionary Chrome extension.</h2>
+                        <button className='px-3 py-2 bg-slate-50 text-black hover:bg-black hover:text-white rounded-3xl lg:text-xl'>
+                            <i class="ri-chrome-fill font-extralight text-lg mr-2"></i>
+                            Get chrome Extension
+                        </button>
+                    </div>
+                </div>
+                {/* Section 4  */}
+                <div className='mx-4'>
+                    <div className='p-3 bg-[#050405] border-[1px] border-slate-500 rounded-2xl  md:mx-[50px] lg:mx-[100px] xl:mx[130px]'>
+                        <div className='sm:p-5'>
+                            <h1 className='mb-4 text-4xl font-medium text-slate-300 md:text-5xl'>What is C-Form?</h1>
+                            <div className='mb-4 flex flex-col gap-3'>
+                                <p className='text-xl text-slate-300 md:text-2xl'>In India, a <span className='text-white underline font-semibold'>C-Form</span> is a mandatory document required under the Foreigners’ Registration Act, 1946.</p>
+                                <p className='text-xl text-slate-300 md:text-2xl'>It serves as a record of the stay of foreign nationals within the country.</p>
+                                <p className='text-xl text-slate-300 md:text-2xl'>Hotels, guesthouses, and other accommodation providers are legally obligated to submit C-Forms to local authorities for every foreign guest’s stay.</p>
+                            </div>
+                            <button className='px-4 py-2 bg-black border-[1px] border-slate-800 rounded-3xl hover:bg-white hover:text-black'>
+                                Read More
+                            </button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
